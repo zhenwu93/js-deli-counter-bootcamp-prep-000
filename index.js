197c03846a4ebc89633a9075d54220be04dc4c8e
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-24 21:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(deliLine) {
 }
 
 function currentLine(line) {
-  if(line > line.length) {
+  if(line.length > 0) {
     return "The line is currently: " + line
   } else {
     return "The line is currently empty."
