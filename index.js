309d99f5d0990849504b6c0178af3a744f8654b2
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-24 16:51:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, "customerName") {
+  for(katzDeliLine = 1; katzDeliLine < katzDeliLine.length; katzDeliLine++;) {
+    return katzDeli.katzDeliLine("customerName");
+  }
+}
+
+function nowServing(katzDeliLine) {
+
+}
+
+function currentLine(katzDeliLine) {
+
+}
